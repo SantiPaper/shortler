7c3d6f970946593d0c4a7486f232d6e479f438c5
SantiPaper <gonsanto07@yahoo.com>
2024-03-06 15:08:02 -0300

    fix index

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,12 @@
 import app from "./app.js";
 import { sequelize } from "./db/db.js";
 
+import "./models/shortUrls.js";
+
 const port = 3000;
 async function main() {
   try {
-    await sequelize.authenticate();
+    await sequelize.sync();
     console.log("Conectado");
     app.listen(port);
     console.log(`Servidor escuchando en ${port}`);
