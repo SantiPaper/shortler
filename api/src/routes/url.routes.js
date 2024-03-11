6be6b6ad423ddda02c241798e377d356ad07243f
SantiPaper <gonsanto07@yahoo.com>
2024-03-11 15:17:40 -0300

    add getSingleUrl route

--- api/src/routes/url.routes.js
+++ api/src/routes/url.routes.js
@@ -1,8 +1,14 @@
-import { getUrls, createUrl } from "../controllers/url.controller.js";
+import {
+  getUrls,
+  createUrl,
+  getSingleUrl,
+} from "../controllers/url.controller.js";
+
 import { Router } from "express";
 const router = Router();
 
 router.get("/url", getUrls);
+router.get("/url/:short_url", getSingleUrl);
 router.post("/url", createUrl);
 
 export default router;
