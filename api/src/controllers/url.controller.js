beea99704cebc919eddbb4ea8954195916ae59ee
SantiPaper <gonsanto07@yahoo.com>
2024-03-11 15:16:52 -0300

    update controller

--- api/src/controllers/url.controller.js
+++ api/src/controllers/url.controller.js
@@ -1,11 +1,40 @@
 import { shortenerUrl } from "../utils/functions.js";
+import { shortUrls } from "../models/shortUrls.js";
 
-export const getUrls = (req, res) => {
-  res.send("getting projects");
+export const getUrls = async (req, res) => {
+  const data = await shortUrls.findAll();
+  if (data.length !== 0) {
+    res.status(200).json(data);
+  } else {
+    res.status(400).send("Data is null");
+  }
+};
+
+export const getSingleUrl = async (req, res) => {
+  const shorterUrl = req.params.short_url;
+  const url = await shortUrls.findOne({ where: { short_url: shorterUrl } });
+  if (url !== null) {
+    res.redirect("https://www.youtube.com/watch?v=4WvX9dBjiJo");
+  } else {
+    res.status(404).json({ error: "Que haces pancho" });
+  }
 };
 
 export const createUrl = async (req, res) => {
-  const { id, original_url } = req.body;
-  const asd = await shortenerUrl();
-  res.send(asd);
+  const { original_url, name } = await req.body;
+  if (!original_url) {
+    return res.status(400).json({ error: "Url is required" });
+  }
+  const shortUrl = await shortenerUrl();
+
+  if ((shortUrl, original_url, name)) {
+    const newData = await shortUrls.create({
+      original_url: original_url,
+      short_url: shortUrl,
+      name: name,
+    });
+    res.send(newData);
+  } else {
+    return res.status(400).json({ error: "Error url shortener" });
+  }
 };
