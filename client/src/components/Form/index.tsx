2d7038ed37f23bbabe14c03d17ff8581dd62b055
SantiPaper <gonsanto07@yahoo.com>
2024-03-13 15:49:12 -0300

    change form component

--- client/src/components/Form/index.tsx
+++ client/src/components/Form/index.tsx
@@ -1,29 +1,54 @@
 import style from "./style.module.css";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { FormEvent } from "react";
 
-export const Form = () => {
-  const [link, setLink] = useState<string>("");
-  const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
-    setLink(ev.target.value);
-  };
+type Props = {
+  onSubmit: (name: string, link: string) => void;
+};
 
+export const Form = ({ onSubmit }: Props) => {
   const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
-    console.log(link);
+    const form = ev.currentTarget;
+    const formData = new FormData(form);
+    const name = formData.get("name")!.toString();
+    const link = formData.get("link")!.toString();
+
+    const linkOnSubmit = {
+      name,
+      link,
+    };
+
+    const valuesGift = Object.values(linkOnSubmit);
+
+    if (valuesGift.some((v) => !v)) {
+      return;
+    }
+    onSubmit(linkOnSubmit.name, linkOnSubmit.link);
+    form.reset();
   };
   return (
     <div className={style.container}>
       <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Link para acortar"
-          type="text"
-          id="link"
-          className={style.form__input}
-          onChange={handleChange}
-        />
-        <label className="sr-only" htmlFor="link">
-          Link para acortar
-        </label>
+        <div>
+          <label htmlFor="name">Nombre</label>
+          <input
+            className={style.form__input}
+            type="text"
+            id="name"
+            placeholder="Ej: Santiago linkedin"
+            name="name"
+          />
+        </div>
+        <div>
+          <label htmlFor="link">Link para acortar</label>
+          <input
+            className={style.form__input}
+            placeholder="Link para acortar"
+            type="text"
+            id="link"
+            name="link"
+          />
+        </div>
         <button className={style.form__button}>Acortar</button>
       </form>
     </div>
