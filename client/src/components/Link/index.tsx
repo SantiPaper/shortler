aee051a7ccf660d962989d53875e86d45288c285
SantiPaper <gonsanto07@yahoo.com>
2024-03-15 17:38:32 -0300

    change link component

--- client/src/components/Link/index.tsx
+++ client/src/components/Link/index.tsx
@@ -1,27 +1,35 @@
 import style from "./style.module.css";
+import { getNewDate } from "../../../utils/getNewDate";
 
 type Props = {
   name: string;
   link: string;
   shorter: string;
+  createAt: string;
 };
 
-export const Link = ({ name, link, shorter }: Props) => {
+export const Link = ({ name, link, shorter, createAt }: Props) => {
+  const date = new Date(createAt);
+
+  const dateString = getNewDate(date);
+
+  const newShortUrl = `${import.meta.env.VITE_DB_URL}${shorter}`;
+
   return (
     <tr className={style.links__tr}>
       <td className={style.links__td}>
         <h3 className={style.title}>{name}</h3>
       </td>
       <td className={style.links__td}>
-        <a href="#">
-          {"https://www.shortler/"}
+        <a href={newShortUrl}>
+          {import.meta.env.VITE_DB_URL}
           {shorter}
         </a>
       </td>
       <td className={style.links__td}>
         <a href={link}>{link}</a>
       </td>
-      <td className={style.links__td}>2023</td>
+      <td className={style.links__td}>{dateString}</td>
     </tr>
   );
 };
