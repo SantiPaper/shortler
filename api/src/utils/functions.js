4865a663f106fd148cf560c11546eb95420c7966
SantiPaper <gonsanto07@yahoo.com>
2024-03-16 18:11:48 -0300

    update function api

--- api/src/utils/functions.js
+++ api/src/utils/functions.js
@@ -1,3 +1,11 @@
-export const shortenerUrl = () => {
-  return Math.random().toString(36).substring(6);
+import { shortUrls } from "../models/shortUrls.js";
+
+export const shortenerUrl = async () => {
+  const shortUrl = Math.random().toString(36).substring(6);
+  const url = await shortUrls.findOne({ where: { short_url: shortUrl } });
+  if (!url) {
+    return shortUrl;
+  } else {
+    shortenerUrl();
+  }
 };
