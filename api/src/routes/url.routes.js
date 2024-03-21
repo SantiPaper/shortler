1009214669fd3c4ee0a239c2ff15e63648873325
SantiPaper <gonsanto07@yahoo.com>
2024-03-21 12:02:24 -0300

    change base url

--- api/src/routes/url.routes.js
+++ api/src/routes/url.routes.js
@@ -8,9 +8,9 @@ import {
 import { Router } from "express";
 const router = Router();
 
-router.get("/url", getUrls);
-router.get("/url/:short_url", getSingleUrl);
-router.post("/url", createUrl);
-router.delete("/url/:short_url", deleteUrl);
+router.get("/", getUrls);
+router.get("/:short_url", getSingleUrl);
+router.post("/", createUrl);
+router.delete("/:short_url", deleteUrl);
 
 export default router;
