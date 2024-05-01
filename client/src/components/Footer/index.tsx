648029917bb5192bbce7074133cb26eb0933c177
SantiPaper <gonsanto07@yahoo.com>
2024-05-01 17:22:14 -0300

    remove wrong label

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -3,14 +3,12 @@ import style from "./style.module.css";
 export const Footer = () => {
   return (
     <footer className={style.footer}>
-      <footer className={style.footer}>
-        <h3>
-          Hecho por{" "}
-          <a href="https://www.linkedin.com/in/santiagovitelli/">
-            Santiago Vitelli ❤️
-          </a>
-        </h3>
-      </footer>
+      <h3>
+        Hecho por{" "}
+        <a href="https://www.linkedin.com/in/santiagovitelli/">
+          Santiago Vitelli ❤️
+        </a>
+      </h3>
     </footer>
   );
 };
