2a477120ef081862fee980d87bc3e8b2140f656d
SantiPaper <gonsanto07@yahoo.com>
2024-05-22 23:14:11 -0300

    Changes links and link component

--- client/src/components/Link/index.tsx
+++ client/src/components/Link/index.tsx
@@ -19,9 +19,7 @@ export const Link = ({ name, link, shorter, createAt, onDelete }: Props) => {
 
   return (
     <tr className={style.links__tr}>
-      <td className={style.links__td}>
-        <h3 className={style.title}>{name}</h3>
-      </td>
+      <td className={style.links__td}>{name}</td>
       <td className={style.links__td}>
         <a href={newShortUrl}>
           {import.meta.env.VITE_DB_URL}
@@ -35,7 +33,15 @@ export const Link = ({ name, link, shorter, createAt, onDelete }: Props) => {
         <GetNewDate date={date} />
       </td>
       <td className={style.links__td}>
-        <button onClick={handleDelete}>Eliminar</button>
+        <button aria-label="Eliminar" onClick={handleDelete}>
+          <svg viewBox="0 0 20 20" fill="currentColor">
+            <path
+              fill-rule="evenodd"
+              d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
       </td>
     </tr>
   );
--- client/src/components/Links/index.tsx
+++ client/src/components/Links/index.tsx
@@ -15,58 +15,58 @@ type Props = {
 export const Links = ({ data, onDelete, loading, dataStorage }: Props) => {
   const { user, isLoading } = useAuth0();
 
+  if (loading) {
+    return <StatusText text="Cargando..." />;
+  }
+
+  if ((user && !data.length) || (!user && !dataStorage.length)) {
+    return <StatusText icon text="No tienes urls acortadas" />;
+  }
+
   return (
     <div className={style.container}>
-      {loading ? (
-        <StatusText text="Cargando" />
-      ) : (
-        <>
-          {!isLoading && !user && dataStorage.length === 1 && <Warn />}
-          {(user && data.length !== 0) ||
-          (!user && dataStorage.length !== 0) ? (
-            <div className={style.container__links}>
-              <table cellSpacing={0} className={style.links}>
-                <thead>
-                  <tr>
-                    <th>Nombre</th>
-                    <th>Url acortada</th>
-                    <th>Url original</th>
-                    <th>Creada</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {user &&
-                    data.length !== 0 &&
-                    data.map((link) => (
-                      <Link
-                        link={link.original_url}
-                        name={link.name}
-                        key={link.short_url}
-                        shorter={link.short_url}
-                        createAt={link.createdAt}
-                        onDelete={onDelete}
-                      />
-                    ))}
-                  {!user &&
-                    dataStorage.map((link) => (
-                      <Link
-                        link={link.original_url}
-                        name={link.name}
-                        key={link.short_url}
-                        shorter={link.short_url}
-                        createAt={link.createdAt}
-                        onDelete={onDelete}
-                      />
-                    ))}
-                </tbody>
-              </table>
-            </div>
-          ) : (
-            <StatusText text="No tienes urls acortadas" />
-          )}
-        </>
-      )}
+      <>
+        {!isLoading && !user && dataStorage.length === 1 && <Warn />}
+
+        <div className={style.container__links}>
+          <table cellSpacing={0} className={style.links}>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Url acortada</th>
+                <th>Url original</th>
+                <th>Creada</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {user &&
+                data.length !== 0 &&
+                data.map((link) => (
+                  <Link
+                    link={link.original_url}
+                    name={link.name}
+                    key={link.short_url}
+                    shorter={link.short_url}
+                    createAt={link.createdAt}
+                    onDelete={onDelete}
+                  />
+                ))}
+              {!user &&
+                dataStorage.map((link) => (
+                  <Link
+                    link={link.original_url}
+                    name={link.name}
+                    key={link.short_url}
+                    shorter={link.short_url}
+                    createAt={link.createdAt}
+                    onDelete={onDelete}
+                  />
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </>
     </div>
   );
 };
