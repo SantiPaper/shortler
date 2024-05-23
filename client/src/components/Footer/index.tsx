547ffd9636b1e9f8a2a65791408ae370cacb278b
SantiPaper <gonsanto07@yahoo.com>
2024-05-22 23:17:39 -0300

    Change footer component

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -3,12 +3,13 @@ import style from "./style.module.css";
 export const Footer = () => {
   return (
     <footer className={style.footer}>
-      <h3>
+      <p>
         Hecho por{" "}
         <a href="https://www.linkedin.com/in/santiagovitelli/">
-          Santiago Vitelli ❤️
-        </a>
-      </h3>
+          Santiago Vitelli
+        </a>{" "}
+        🧑‍💻
+      </p>
     </footer>
   );
 };
